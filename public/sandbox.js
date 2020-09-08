8735be0d1b571aee0cea9634af5c7ae7afe99eda
SepSol <SR-75@live.com>
2020-09-07 23:46:14 -0700

    function and void type in typescript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('testing');
+function fOne() {
+    console.log('one');
+}
+function fTwo() {
+    console.log('two');
+}
+var fThree;
+// fThree = 3;
+fThree = function () { return console.log('three'); };
+var fFour = function () {
+    console.log('four');
+};
+var fFive = function (a, b) {
+    console.log(a + b);
+};
+var fSix = function (a, b, c) {
+    console.log(a + b);
+};
+var fSeven = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b + c);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,27 @@
-console.log('testing');
+function fOne() {
+  console.log('one');
+}
+
+function fTwo(): void {
+  console.log('two');
+}
+
+let fThree: Function;
+// fThree = 3;
+fThree = () => console.log('three');
+
+const fFour: Function = (): void => {
+  console.log('four');
+};
+
+const fFive: Function = (a: number, b: number): void => {
+  console.log(a + b);
+};
+
+const fSix = (a: number, b: number, c?: number | string) => {
+  console.log(a + b);
+};
+
+const fSeven = (a: number, b: number, c: number = 10) => {
+  console.log(a + b + c);
+};
